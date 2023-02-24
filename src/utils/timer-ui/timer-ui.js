655b9b37932e918c052d3f53155095285e85359a
ypjun100 <ypjun100@gmail.com>
2023-02-24 12:05:41 +0900

    Font changed 'Futura' to 'Jost'.

--- src/utils/timer-ui/timer-ui.js
+++ src/utils/timer-ui/timer-ui.js
@@ -1,4 +1,6 @@
 import { Text, BLEND_MODES, Container } from 'pixi.js';
+import FontFaceObserver from 'fontfaceobserver';
+
 import alarm from '../../assets/alarm.mp3';
 
 export class TimerUIContainer {
@@ -11,58 +13,63 @@ export class TimerUIContainer {
         this.alarmSound = new Audio();
         this.alarmSound.autoplay = true;
 
-        // default text style
-        const defaultTextStyle = {
-            fontFamily: 'Futura',
-            fill: '#8fc2ff',
-            fontSize: 22
-        };
+        var fontN = new FontFaceObserver('JostN'); // normal font
+        var fontB = new FontFaceObserver('JostB'); // bold font
 
-        // main timer text
-        this.timerText = new Text(this.secondsToTime(this.currentSeconds), Object.assign({}, defaultTextStyle, {
-            fontSize: 92,
-            fontWeight: 'bold',
-            align: 'center'
-        }));
-        this.timerText.alpha = 0.9;
-        this.timerText.anchor.set(0.5, 0.5);
-        this.timerText.position.set(...this.getPosition('center', width, height));
-        this.timerText.blendMode = BLEND_MODES.XOR;
+        Promise.all([fontN.load(), fontB.load()]).then(() => {
+            // default text style
+            const defaultTextStyle = {
+                fontFamily: 'JostN',
+                fill: '#8fc2ff',
+                fontSize: 22
+            };
 
-        // start time text
-        this.startTimeText = new Text(this.secondsToTime(this.initialSeconds), Object.assign({}, defaultTextStyle, { align: 'left' }));
-        this.startTimeText.alpha = 0.9;
-        this.startTimeText.anchor.set(0, 1);
-        this.startTimeText.position.set(...this.getPosition('left-top', width, height));
-        this.startTimeText.blendMode = BLEND_MODES.XOR;
+            // main timer text
+            this.timerText = new Text(this.secondsToTime(this.currentSeconds), Object.assign({}, defaultTextStyle, {
+                fontFamily: 'JostB',
+                fontSize: 92,
+                align: 'center'
+            }));
+            this.timerText.alpha = 0.9;
+            this.timerText.anchor.set(0.5, 0.5);
+            this.timerText.position.set(...this.getPosition('center', width, height));
+            this.timerText.blendMode = BLEND_MODES.XOR;
 
-        // number of times text
-        this.numberOfTimesText = new Text(`${this.numberOfTimes} times`, Object.assign({}, defaultTextStyle, { align: 'right' }));
-        this.numberOfTimesText.alpha = 0.9;
-        this.numberOfTimesText.anchor.set(1, 1);
-        this.numberOfTimesText.position.set(...this.getPosition('right-top', width, height));
-        this.numberOfTimesText.blendMode = BLEND_MODES.XOR;
+            // start time text
+            this.startTimeText = new Text(this.secondsToTime(this.initialSeconds), Object.assign({}, defaultTextStyle, { align: 'left' }));
+            this.startTimeText.alpha = 0.9;
+            this.startTimeText.anchor.set(0, 1);
+            this.startTimeText.position.set(...this.getPosition('left-top', width, height));
+            this.startTimeText.blendMode = BLEND_MODES.XOR;
 
-        // reset button
-        this.resetButton = new Text('reset', Object.assign({}, defaultTextStyle, { align: 'left' }));
-        this.resetButton.anchor.set(0, 0);
-        this.resetButton.position.set(...this.getPosition('left-bottom', width, height));
-        this.resetButton.blendMode = BLEND_MODES.XOR;
-        this.resetButton.interactive = true;
-        this.resetButton.cursor = 'pointer';
-        this.resetButton.on('pointerdown', this.timerReset.bind(this));
+            // number of times text
+            this.numberOfTimesText = new Text(`${this.numberOfTimes} times`, Object.assign({}, defaultTextStyle, { align: 'right' }));
+            this.numberOfTimesText.alpha = 0.9;
+            this.numberOfTimesText.anchor.set(1, 1);
+            this.numberOfTimesText.position.set(...this.getPosition('right-top', width, height));
+            this.numberOfTimesText.blendMode = BLEND_MODES.XOR;
 
-        // start/pause button
-        this.startPauseButton = new Text('start', Object.assign({}, defaultTextStyle, { align: 'right' }));
-        this.startPauseButton.anchor.set(1, 0);
-        this.startPauseButton.position.set(...this.getPosition('right-bottom', width, height));
-        this.startPauseButton.blendMode = BLEND_MODES.XOR;
-        this.startPauseButton.interactive = true;
-        this.startPauseButton.cursor = 'pointer';
-        this.startPauseButton.on('pointerdown', this.timerStartPause.bind(this));
+            // reset button
+            this.resetButton = new Text('reset', Object.assign({}, defaultTextStyle, { align: 'left' }));
+            this.resetButton.anchor.set(0, 0);
+            this.resetButton.position.set(...this.getPosition('left-bottom', width, height));
+            this.resetButton.blendMode = BLEND_MODES.XOR;
+            this.resetButton.interactive = true;
+            this.resetButton.cursor = 'pointer';
+            this.resetButton.on('pointerdown', this.timerReset.bind(this));
 
-        this.container.addChild(this.timerText, this.startTimeText, this.numberOfTimesText, this.resetButton, this.startPauseButton);
+            // start/pause button
+            this.startPauseButton = new Text('start', Object.assign({}, defaultTextStyle, { align: 'right' }));
+            this.startPauseButton.anchor.set(1, 0);
+            this.startPauseButton.position.set(...this.getPosition('right-bottom', width, height));
+            this.startPauseButton.blendMode = BLEND_MODES.XOR;
+            this.startPauseButton.interactive = true;
+            this.startPauseButton.cursor = 'pointer';
+            this.startPauseButton.on('pointerdown', this.timerStartPause.bind(this));
 
+            this.container.addChild(this.timerText, this.startTimeText, this.numberOfTimesText, this.resetButton, this.startPauseButton);
+        });
+        
         // fps
         this.fps = new Text("fps : 00");
         this.fps.position.set(0, 0);
@@ -71,15 +78,15 @@ export class TimerUIContainer {
 
     getPosition(position, width, height) {
         if(position === "left-top")
-            return [width / 2 - 140, height / 2 - 70];
+            return [width / 2 - 130, height / 2 - 70];
         else if(position === "right-top")
-            return [width / 2 + 140, height / 2 - 70]
+            return [width / 2 + 130, height / 2 - 70]
         else if(position === "center")
             return [width / 2, height / 2]
         else if(position === "left-bottom")
-            return [width / 2 - 140, height / 2 + 70]
+            return [width / 2 - 130, height / 2 + 70]
         else if(position === "right-bottom")
-            return [width / 2 + 140, height / 2 + 70]
+            return [width / 2 + 130, height / 2 + 70]
     }
 
     rerender() {
